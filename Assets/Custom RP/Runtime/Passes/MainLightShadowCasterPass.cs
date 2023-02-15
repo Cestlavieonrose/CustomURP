012cf41d3d1238f2b51bff697bfcae29f18f889b
tufeng060 <diaolp1992@gmail.com>
2023-02-15 20:34:09 +0800

    恢复乱码

--- Assets/Custom RP/Runtime/Passes/MainLightShadowCasterPass.cs
+++ Assets/Custom RP/Runtime/Passes/MainLightShadowCasterPass.cs
@@ -9,20 +9,20 @@ namespace UnityEngine.Rendering.Custom.Internal
     {
         private static class MainLightShadowConstantBuffer
         {
-            public static int _WorldToShadow; //���絽��Ӱ�ռ��ת������
-            public static int _ShadowParams;//��Ӱ������light.shadowStrength, softShadowsProp, oneOverFadeDist, minusStartFade
-            public static int _CascadeShadowSplitSpheres0;//ÿ����Χ������� center��xyz�� radius��w
+            public static int _WorldToShadow; //世界到阴影空间的转换矩阵
+            public static int _ShadowParams;//阴影参数：light.shadowStrength, softShadowsProp, oneOverFadeDist, minusStartFade
+            public static int _CascadeShadowSplitSpheres0;//每个包围球的数据 center：xyz， radius：w
             public static int _CascadeShadowSplitSpheres1;
             public static int _CascadeShadowSplitSpheres2;
             public static int _CascadeShadowSplitSpheres3;
-            public static int _CascadeShadowSplitSphereRadii;//���Ǻ�����Ҫ����ɫ�����ж���������ƬԪ�Ƿ��ڰ�Χ���У�����ͨ����ƬԪ�����ľ����ƽ��������뾶��ƽ�����Ƚϣ����Ǵ�������֮ǰ�ȼ��������뾶��ƽ�����Ͳ���������ɫ���м����ˡ�
+            public static int _CascadeShadowSplitSphereRadii;//我们后续需要在着色器中判断物体表面的片元是否在包围球中，可以通过该片元到球心距离的平方和球体半径的平方来比较，我们传递数据之前先计算好球体半径的平方，就不用再在着色器中计算了。
             public static int _ShadowOffset0;
             public static int _ShadowOffset1;
             public static int _ShadowOffset2;
             public static int _ShadowOffset3;
             public static int _ShadowmapSize;
         }
-        //shadow��ͼ��λ��
+        //shadow贴图的位数
         const int k_ShadowmapBufferBits = 16;
         RenderTargetHandle m_MainLightShadowmap;
         RenderTexture m_MainLightShadowmapTexture;
@@ -32,11 +32,11 @@ namespace UnityEngine.Rendering.Custom.Internal
         int m_ShadowCasterCascadesCount;
 
         ShadowSliceData[] m_CascadeSlices;
-        //����ÿ��������Χ������cullingSphere��Ϊһ��vector4��ǰ��λ���������꣬���һλ�ǰ뾶
-        //����Ϊ����Ⱦʱ���������λ�ó�������Իἰʱ�ı䣬����ÿ���㼶�����ӽ��嶼�᲻�ϱ任�����ӽ����������Χ��ҲҪ���ű仯��
-        //���������ܵ��³���ǰ����֡������Χ�з���ͻ�䣬�����������ɵ���Ӱ��ͼ����Ч�ֱ��ʿ�������������֡�з���ͻ�䣬������Ӱ�������⣬
-        //��������ǰѰ�Χ�и�Ϊ��Χ�򣬰�Χ���������ӽ���ı仯��������С�ı仯�̶�����ڰ�Χ����˵С�ܶ�
-        //��ķ�������޹أ������������еķ���ⶼʹ����ͬ�İ�Χ��
+        //设置每个级联包围球数据cullingSphere，为一个vector4，前三位是球心坐标，最后一位是半径
+        //但因为在渲染时，摄像机的位置朝向等属性会及时改变，所以每个层级的子视截体都会不断变换，子视截体的轴对齐包围盒也要跟着变化，
+        //但这样可能导致出现前后两帧轴对齐包围盒发生突变，进而导致生成的阴影贴图的有效分辨率可能在连续的两帧中发生突变，产生阴影抖动问题，
+        //解决方案是把包围盒改为包围球，包围球随着子视截体的变化而发生大小的变化程度相对于包围盒来说小很多
+        //光的方向和球无关，所以我们所有的方向光都使用相同的包围球。
         Vector4[] m_CascadeSplitDistances;
         Matrix4x4[] m_MainLightShadowMatrices;
         bool m_SupportsBoxFilterForShadows;
@@ -76,8 +76,8 @@ namespace UnityEngine.Rendering.Custom.Internal
         {
             using var profScope = new ProfilingScope(null, m_ProfilingSetupSampler);
 
-            //�Ƿ�֧����Ҫ��������������
-            //1.ϵͳ�Ƿ�֧��  2.asset�Ƿ�ѡ֧�� 3 light����Ƿ�֧��
+            //是否支持需要满足三个条件：
+            //1.系统是否支持  2.asset是否勾选支持 3 light组件是否支持
             if (!renderingData.shadowData.supportsMainLightShadows)
                 return false;
 
@@ -95,7 +95,7 @@ namespace UnityEngine.Rendering.Custom.Internal
             {
                 Debug.LogWarning("Only directional lights are supported as main light.");
             }
-            //�жϵ�ǰ�ƹ�û�������κ�����
+            //判断当前灯光没有照射任何物体
             Bounds bounds;
             if (!renderingData.cullResults.GetShadowCasterBounds(shadowLightIndex, out bounds))
                 return false;
@@ -108,13 +108,13 @@ namespace UnityEngine.Rendering.Custom.Internal
                 renderingData.shadowData.mainLightShadowmapHeight >> 1 :
                 renderingData.shadowData.mainLightShadowmapHeight;
 
-//��Ӱ��ͼ����Ҳ��һ�����ͼ������¼�˴ӹ�Դλ�ó��������ܿ����ĳ����о���������ı���λ�ã������Ϣ����
-//���Ƿ���Ⲣû��һ����ʵλ�ã�����Ҫ�������ҳ����ķ���ƥ�����ͼ��ͶӰ���󣬲�������һ���ü��ռ�������壬
-//���������������Դ��Ӱ����Ӱ���Ŀɼ������ص�����Щ���ݵĻ�ȡ���ǲ����Լ�ȥʵ�֣�
-//����ֱ�ӵ���cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives������
-//����Ҫ9����������1���ǿɼ������������2��3��4����������������Ӱ�������ݣ��������ǻᴦ��������5����������Ӱ��ͼ�ĳߴ磬
-//��6����������Ӱ��ƽ��ƫ�ƣ������Ⱥ�������������������������������һ������ͼ����һ����ͶӰ����һ����ShadowSplitData����
-//�������йظ�����Ӱ�ָ�綨���������޳���Ϣ��
+//阴影贴图本质也是一张深度图，它记录了从光源位置出发，到能看到的场景中距离它最近的表面位置（深度信息）。
+//但是方向光并没有一个真实位置，我们要做地是找出与光的方向匹配的视图和投影矩阵，并给我们一个裁剪空间的立方体，
+//该立方体与包含光源阴影的摄影机的可见区域重叠，这些数据的获取我们不用自己去实现，
+//可以直接调用cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives方法，
+//它需要9个参数。第1个是可见光的索引，第2、3、4个参数用于设置阴影级联数据，后面我们会处理它，第5个参数是阴影贴图的尺寸，
+//第6个参数是阴影近平面偏移，我们先忽略它。最后三个参数都是输出参数，一个是视图矩阵，一个是投影矩阵，一个是ShadowSplitData对象，
+//它描述有关给定阴影分割（如定向级联）的剔除信息。
             for (int cascadeIndex = 0; cascadeIndex < m_ShadowCasterCascadesCount; ++cascadeIndex)
             {
                 bool success = ShadowUtils.ExtractDirectionalLightMatrix(ref renderingData.cullResults, ref renderingData.shadowData,
@@ -143,7 +143,7 @@ namespace UnityEngine.Rendering.Custom.Internal
         }
 
 
-        //����RT����ָ������������Ӱ��ͼ
+        //创建RT，并指定该类型是阴影贴图
         public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
         {
             m_MainLightShadowmapTexture = ShadowUtils.GetTemporaryShadowTexture(m_ShadowmapWidth,
@@ -172,7 +172,7 @@ namespace UnityEngine.Rendering.Custom.Internal
                     var splitData = settings.splitData;
                     splitData.cullingSphere = m_CascadeSplitDistances[cascadeIndex];
                     settings.splitData = splitData;
-                    //�������ƫ��
+                    //调整深度偏差
                     Vector4 shadowBias = ShadowUtils.GetShadowBias(ref shadowLight, shadowLightIndex, ref shadowData, m_CascadeSlices[cascadeIndex].projectionMatrix, m_CascadeSlices[cascadeIndex].resolution);
                     ShadowUtils.SetupShadowCasterConstantBuffer(cmd, ref shadowLight, shadowBias);
                     ShadowUtils.RenderShadowSlice(cmd, ref context, ref m_CascadeSlices[cascadeIndex],
@@ -238,9 +238,9 @@ namespace UnityEngine.Rendering.Custom.Internal
             float oneOverFadeDist = 1/(m_MaxShadowDistance - startFade);
             float minusStartFade = -startFade * oneOverFadeDist;
 
-            //������Ӱ��ͼ
+            //设置阴影贴图
             cmd.SetGlobalTexture(m_MainLightShadowmap.id, m_MainLightShadowmapTexture);
-            //����Ⱦ����Ӱ�󣬵���buffer.SetGlobalMatrixArray������ת�������͵�GPU��
+            //当渲染完阴影后，调用buffer.SetGlobalMatrixArray方法将转换矩阵发送到GPU。
             cmd.SetGlobalMatrixArray(MainLightShadowConstantBuffer._WorldToShadow, m_MainLightShadowMatrices);
             cmd.SetGlobalVector(MainLightShadowConstantBuffer._ShadowParams, new Vector4(light.shadowStrength, softShadowsProp, oneOverFadeDist, minusStartFade));
 
